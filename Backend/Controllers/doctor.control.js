34b364bc01aefa8fad4f8ba1409bada5a65f2464
elhussienawad1 <hussienmohamed2004@gmail.com>
2025-02-26 22:53:17 +0200

    patient and doc

--- Backend/Controllers/doctor.control.js
+++ Backend/Controllers/doctor.control.js
@@ -1,16 +1,74 @@
 const {Doctor, User} = require('../models.js')
 
-const editInfo =async(req,res) =>{
 
-}
+const register = async (req, res) => {
+    try {
+        const user = new User({
+            fullname: req.body.fullname,
+            email: req.body.email,
+            phoneNumber: req.body.phoneNumber,
+            role: "Doctor",
+            gender: req.body.gender,
+            password: req.body.password,
+            birthdate: req.body.birthdate
+        });
+
+        await user.save();  
+
+        const newDoctor = new Doctor({
+            user: user._id,   
+            specialization: req.body.specialization
+        });
+
+        await newDoctor.save();  
+
+        res.json({ user, doctor: newDoctor });  
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+
+const editInfo =async(req,res) =>{
+    try{
+        const { birthdate, email, phoneNumber }= req.body;
+        const userId = req.params.id;
+        const updatedDoctor = await User.findByIdAndUpdate(
+            userId,
+            { birthdate, email, phoneNumber },
+            { new : true } 
+        );
+        if (!updatedDoctor) {
+            return res.status(404).json({ msg: "Doctor not found" });
+        }
+        res.json({ msg: "Doctor info updated successfully", doctor: updatedDoctor });
+    } 
+    catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+    }
+};
 
 const viewRelatedPatients = async(req,res) =>{
+    try{
+        const userId = req.params.id;
+        const patients = await User.find({ doctorId: userId }).select("-password");
 
-}
+    if (!patients.length) {
+      return res.status(404).json({ msg: "No patients assigned to you." });
+    }
+
+    res.json({ msg: "Patients retrieved successfully", patients });
+    }   
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ msg:"Server error"});
+    }
+};
 
 
 module.exports = {
     editInfo,
-    viewRelatedPatients
-
+    viewRelatedPatients,
+    register
 }
--- Backend/Routes/doctor.routes.js
+++ Backend/Routes/doctor.routes.js
@@ -1,12 +1,14 @@
 const{
     editInfo,
-    viewRelatedPatients
-
+    viewRelatedPatients,
+    register
 } = require('../Controllers/doctor.control.js')
 
 const express = require('express');
 const router = express.Router();
 
+
+router.post('/register',register)
 router.patch('/edit-info/:id',editInfo);
 router.get('view-related-patients',viewRelatedPatients);
 
